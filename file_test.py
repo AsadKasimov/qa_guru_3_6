2f34ea1558750745204e66df2c16eb091a3988cb
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2022-12-25 11:51:47 +0300

    передел

--- file_test.py
+++ file_test.py
@@ -7,15 +7,22 @@ from openpyxl import load_workbook
 
 # тут только тесты с файлами
 
-# я не знаю почему но пока не заранить work_with_files она не будет читать его здесь
+def test_creat_zip():
+    with zipfile.ZipFile('data/spam.zip', 'w') as myzip:
+        myzip.write('data/some.csv')
+        myzip.write('data/simple_demo.pdf')
+        myzip.write('data/test_with_files.xlsx')
+
+
 def test_zip():
     with zipfile.ZipFile('data/spam.zip', 'r') as myzip:
         assert len(myzip.namelist()) == 3  # проверка сколько файлов входтит в файл
-
         assert 'data/simple_demo.pdf' in myzip
         assert 'data/some.csv' in myzip
         assert 'data/test_with_files.xlsx' in myzip
+    assert 'spam.zip' in 'data'
 
+def test_csv():
     # тут читаем csv файлы
     with open('data/some.csv', newline='') as f:
         for row in csv.reader(f):
@@ -25,24 +32,25 @@ def test_zip():
 
 
 # тут раболта с pdf
-pdf_document = "data/simple_demo.pdf"
-with open(pdf_document, "rb") as filehandle:
-    pdf = PdfFileReader(filehandle)
-    page1 = pdf.getPage(0)
-    text = page1.extractText()
-    # тесты
-    print(re.findall(r'^\w+', text, flags=re.IGNORECASE) == ['Welcome'])
-    text_from = re.findall(r'\w+', text, flags=re.IGNORECASE)
-    assert 'Welcome' in text_from
-    assert 'to' in text_from
-    assert 'Python' in text_from
-    assert len(text_from) == 3
+def test_pdf():
+    pdf_document = "data/simple_demo.pdf"
+    with open(pdf_document, "rb") as filehandle:
+        pdf = PdfFileReader(filehandle)
+        page1 = pdf.getPage(0)
+        text = page1.extractText()
+        # тесты
+        print(re.findall(r'^\w+', text, flags=re.IGNORECASE) == ['Welcome'])
+        text_from = re.findall(r'\w+', text, flags=re.IGNORECASE)
+        assert 'Welcome' in text_from
+        assert 'to' in text_from
+        assert 'Python' in text_from
+        assert len(text_from) == 3
 
-
-# тут читаем exel
-book = load_workbook('data/test_with_files.xlsx')
-sheet = book.active
-ass = sheet.cell(row=3, column=3).value
-assert sheet.max_row == 11
-assert sheet.max_column == 8
-assert ass == 25000
+def test_ex():
+    # тут читаем exel
+    book = load_workbook('data/test_with_files.xlsx')
+    sheet = book.active
+    ass = sheet.cell(row=3, column=3).value
+    assert sheet.max_row == 11
+    assert sheet.max_column == 8
+    assert ass == 25000
